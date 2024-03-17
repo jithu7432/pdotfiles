9af1165f3d768a257467b2ce8c163906ed6e7694
Jithin Johnson <jithu7432@gmail.com>
2024-03-17 13:56:22 +0530

    some fish changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,18 +1,16 @@
 if status is-interactive
-        # fish
-        set fish_greeting
         source ~/.config/fish/aliases.fish
+
+        set fish_greeting
         set -U fish_user_paths ~/.local/bin $fish_user_paths
 
-        # starship
-        starship init fish | source
+        starship init fish | source 
 
-        # fzf
         set -U FZF_DISABLE_KEYBINDINGS 1
         set -U FZF_DEFAULT_COMMAND fd
         set -U DOTNET_ROOT ~/.dotnet
+        set -U DOTNET_CLI_TELEMETRY_OPTOUT 1
 
-        # PATH
         set PATH "$HOME/.cargo/bin/:$PATH"
         set PATH "$HOME/.dotnet:$HOME/.dotnet/tools:$PATH"
         set PATH "$HOME/.yarn/bin/:$PATH"
