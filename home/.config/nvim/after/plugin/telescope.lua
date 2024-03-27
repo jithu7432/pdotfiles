2877ccea18e709775d85eaa37989b8d1c3639785
Jithin Johnson <jithu7432@gmail.com>
2024-03-28 00:38:24 +0530

    moved to nvim-cmp

--- home/.config/nvim/after/plugin/telescope.lua
+++ home/.config/nvim/after/plugin/telescope.lua
@@ -1,5 +1,7 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>ps', function() 
+
+vim.keymap.set('n', '<leader>ps', 
+function() 
 	builtin.grep_string({ search = vim.fn.input("Rg> ") });
 end, {})
