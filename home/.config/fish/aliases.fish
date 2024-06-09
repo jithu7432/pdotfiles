e7fb8ad8e616741fec3a7fb77255d1c05d9e8925
Jithin Johnson <jithu7432@gmail.com>
2024-06-09 11:40:12 +0530

    ADded i3 changes.

--- home/.config/fish/aliases.fish
+++ home/.config/fish/aliases.fish
@@ -1,17 +1,22 @@
 #/usr/bin/fish
 
-alias c='clear'
-alias ccc='curl ip-api.com'
-alias cls='clear'
-alias ee='. .env'
+alias c=clear
+alias cls=clear
+
 alias l='exa -l'
-alias la='ls -Ah'
-alias leet='cd ~/code/leetcode && vim vimconfig'
+alias la='exa -Ah'
 alias ll='exa -la'
 alias ls='exa'
+
+alias ccc='curl ip-api.com'
+alias leet='cd ~/code/leetcode && vim vimconfig'
 alias nau='thunar'
 alias tlmgr='/usr/share/texmf-dist/scripts/texlive/tlmgr.pl --usermode'
 alias touchenv='echo ". .venv/bin/activate.fish" > .env'
+
 alias vimg='vim +G'
 alias webp2gif='magick mogrify -format gif *.webp && rm *.webp'
+
 alias yy='yay -Syu --noconfirm'
+
+alias tt=tmux
