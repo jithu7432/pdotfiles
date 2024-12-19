d74adc72483e7534192fc5c16deb279f35588dda
Jithin Johnson <jithu7432@gmail.com>
2024-12-19 13:20:13 +0530

    - Add npm, node to fish $PATH - tmux now opens new buff at cwd

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -17,5 +17,5 @@ if status is-interactive
 
         set PATH "$HOME/.cargo/bin/:$PATH"
         set PATH "$HOME/.dotnet:$HOME/.dotnet/tools:$PATH"
-        set PATH "$HOME/.npm/bin/:$PATH"
+        set PATH "$HOME/.node/bin/:$PATH"
 end
