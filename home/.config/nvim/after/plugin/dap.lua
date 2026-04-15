f7fb7380fae2cb081cd16648e6c83a3c373eb357
Jithin Johnson <jithu7432@gmail.com>
2026-04-16 00:30:33 +0530

    Added dap for rust

--- home/.config/nvim/after/plugin/dap.lua
+++ home/.config/nvim/after/plugin/dap.lua
@@ -1,54 +1,10 @@
 local dap = require("dap")
-
-local dapui = require("dapui")
-dapui.setup()
-
-require("nvim-dap-virtual-text").setup()
-
-dap.adapters.coreclr = {
-	type = "executable",
-	command = "/home/user/tools/netcoredbg/netcoredbg",
-	args = { "--interpreter=vscode" },
-}
-
-dap.configurations.cs = {
-	{
-		type = "coreclr",
-		name = "launch - netcoredbg",
-		request = "launch",
-		program = function()
-			return vim.fn.input("Path to dll", vim.fn.getcwd() .. "/bin/Debug/", "file")
-		end,
-	},
-}
-
-dap.listeners.before.attach.dapui_config = function()
-	dapui.open()
-end
-dap.listeners.before.launch.dapui_config = function()
-	dapui.open()
-end
-dap.listeners.before.event_terminated.dapui_config = function()
-	dapui.close()
-end
-dap.listeners.before.event_exited.dapui_config = function()
-	dapui.close()
-end
-
-vim.keymap.set("n", "<space>a", dap.toggle_breakpoint)
-
-vim.keymap.set("n", "<F1>", dap.continue)
-vim.keymap.set("n", "<F5>", dap.step_out)
-vim.keymap.set("n", "<F10>", dap.step_over)
-vim.keymap.set("n", "<F11>", dap.step_into)
-vim.keymap.set("n", "<F12>", dap.step_out)
-
 dap.adapters.gdb = {
 	type = "executable",
 	command = "gdb",
 	args = { "--interpreter=dap", "--eval-command", "set print pretty on" },
 }
-local dap = require("dap")
+
 dap.configurations.c = {
 	{
 		name = "Launch",
@@ -85,3 +41,6 @@ dap.configurations.c = {
 		cwd = "${workspaceFolder}",
 	},
 }
+
+dap.configurations.cpp = dap.configurations.c
+dap.configurations.rust = dap.configurations.c
---
+++ home/.config/nvim/after/plugin/dapconfig.lua
@@ -0,0 +1,45 @@
+local dap = require("dap")
+
+local dapui = require("dapui")
+dapui.setup()
+
+require("nvim-dap-virtual-text").setup()
+
+dap.adapters.coreclr = {
+	type = "executable",
+	command = "/home/user/tools/netcoredbg/netcoredbg",
+	args = { "--interpreter=vscode" },
+}
+
+dap.configurations.cs = {
+	{
+		type = "coreclr",
+		name = "launch - netcoredbg",
+		request = "launch",
+		program = function()
+			return vim.fn.input("Path to dll", vim.fn.getcwd() .. "/bin/Debug/", "file")
+		end,
+	},
+}
+
+dap.listeners.before.attach.dapui_config = function()
+	dapui.open()
+end
+dap.listeners.before.launch.dapui_config = function()
+	dapui.open()
+end
+dap.listeners.before.event_terminated.dapui_config = function()
+	dapui.close()
+end
+dap.listeners.before.event_exited.dapui_config = function()
+	dapui.close()
+end
+
+vim.keymap.set("n", "<space>a", dap.toggle_breakpoint)
+
+vim.keymap.set("n", "<F1>", dap.continue)
+vim.keymap.set("n", "<F5>", dap.step_out)
+vim.keymap.set("n", "<F10>", dap.step_over)
+vim.keymap.set("n", "<F11>", dap.step_into)
+vim.keymap.set("n", "<F12>", dap.step_out)
+
