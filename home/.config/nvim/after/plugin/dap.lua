e3ca9dc3c6e2fd76a3b7ccdab0b2906f3b41f7c0
Jithin Johnson <jithu7432@gmail.com>
2026-04-16 23:29:33 +0530

    - vs**** like keybindings for dap - no generic dapconfig, should use .vs****/launch.json

--- home/.config/nvim/after/plugin/dap.lua
+++ home/.config/nvim/after/plugin/dap.lua
@@ -5,42 +5,8 @@ dap.adapters.gdb = {
 	args = { "--interpreter=dap", "--eval-command", "set print pretty on" },
 }
 
-dap.configurations.c = {
-	{
-		name = "Launch",
-		type = "gdb",
-		request = "launch",
-		program = function()
-			return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-		end,
-		args = {}, -- provide arguments if needed
-		cwd = "${workspaceFolder}",
-		stopAtBeginningOfMainSubprogram = false,
-	},
-	{
-		name = "Select and attach to process",
-		type = "gdb",
-		request = "attach",
-		program = function()
-			return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-		end,
-		pid = function()
-			local name = vim.fn.input("Executable name (filter): ")
-			return require("dap.utils").pick_process({ filter = name })
-		end,
-		cwd = "${workspaceFolder}",
-	},
-	{
-		name = "Attach to gdbserver :1234",
-		type = "gdb",
-		request = "attach",
-		target = "localhost:1234",
-		program = function()
-			return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-		end,
-		cwd = "${workspaceFolder}",
-	},
+dap.adapters.coreclr = {
+	type = "executable",
+	command = "/home/user/tools/netcoredbg/netcoredbg",
+	args = { "--interpreter=vscode" },
 }
-
-dap.configurations.cpp = dap.configurations.c
-dap.configurations.rust = dap.configurations.c
--- home/.config/nvim/after/plugin/dapconfig.lua
+++ home/.config/nvim/after/plugin/dapconfig.lua
@@ -5,23 +5,6 @@ dapui.setup()
 
 require("nvim-dap-virtual-text").setup()
 
-dap.adapters.coreclr = {
-	type = "executable",
-	command = "/home/user/tools/netcoredbg/netcoredbg",
-	args = { "--interpreter=vscode" },
-}
-
-dap.configurations.cs = {
-	{
-		type = "coreclr",
-		name = "launch - netcoredbg",
-		request = "launch",
-		program = function()
-			return vim.fn.input("Path to dll", vim.fn.getcwd() .. "/bin/Debug/", "file")
-		end,
-	},
-}
-
 dap.listeners.before.attach.dapui_config = function()
 	dapui.open()
 end
@@ -37,9 +20,11 @@ end
 
 vim.keymap.set("n", "<space>a", dap.toggle_breakpoint)
 
-vim.keymap.set("n", "<F1>", dap.continue)
-vim.keymap.set("n", "<F5>", dap.step_out)
+vim.keymap.set("n", "<F5>", dap.continue)
 vim.keymap.set("n", "<F10>", dap.step_over)
 vim.keymap.set("n", "<F11>", dap.step_into)
 vim.keymap.set("n", "<F12>", dap.step_out)
 
+vim.keymap.set("n", "<leader>d", function()
+	dapui.close()
+end)
