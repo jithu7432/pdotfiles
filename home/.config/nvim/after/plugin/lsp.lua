229396749f2ebb38d1f0ac770df80c7b95e62f80
Jithin Johnson <jithu7432@gmail.com>
2026-07-14 20:28:17 +0530

    feat: lsp astro

--- home/.config/nvim/after/plugin/lsp.lua
+++ home/.config/nvim/after/plugin/lsp.lua
@@ -16,6 +16,19 @@ vim.lsp.enable('yamlls')
 vim.lsp.enable('tailwindcss')
 vim.lsp.enable('html')
 vim.lsp.enable('nginx_language_server')
+vim.lsp.config['astro'] = {
+      init_options = {
+        typescript = {
+          tsdk = '/usr/lib/node_modules/typescript/lib',
+        },
+      },
+    }
+	init_options = {
+		typescript = {
+			tsdk = "/usr/lib/node_modules/typescript/lib",
+		},
+	},
+vim.lsp.enable("astro")
 
 if vim.fn.has('win32') ~= 1 then
 	vim.lsp.enable('bashls')
