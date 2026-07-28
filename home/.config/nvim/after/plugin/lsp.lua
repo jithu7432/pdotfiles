03570616177a4c70e44ad0f37df11876e23bcf8b
Jithin Johnson <jithu7432@gmail.com>
2026-07-28 23:31:49 +0530

    chore: roslyn no background shize

--- home/.config/nvim/after/plugin/lsp.lua
+++ home/.config/nvim/after/plugin/lsp.lua
@@ -1,42 +1,49 @@
-vim.lsp.enable('clangd')
-vim.lsp.enable('digestif')
-vim.lsp.enable('gopls')
-vim.lsp.enable('gradle_ls')
-vim.lsp.enable('lemminx')
-vim.lsp.enable('stylua')
-vim.lsp.enable('neocmake')
-vim.lsp.enable('zuban')
-vim.lsp.enable('rust_analyzer')
-vim.lsp.enable('taplo')
-vim.lsp.enable('tsgo')
-vim.lsp.enable('eslint')
-vim.lsp.enable('kotlin_lsp')
-vim.lsp.enable('roslyn_ls')
-vim.lsp.enable('yamlls')
-vim.lsp.enable('tailwindcss')
-vim.lsp.enable('html')
-vim.lsp.enable('nginx_language_server')
-vim.lsp.config['astro'] = {
-      init_options = {
-        typescript = {
-          tsdk = '/usr/lib/node_modules/typescript/lib',
-        },
-      },
-    }
+vim.lsp.enable("clangd")
+vim.lsp.enable("digestif")
+vim.lsp.enable("gopls")
+vim.lsp.enable("gradle_ls")
+vim.lsp.enable("lemminx")
+vim.lsp.enable("stylua")
+vim.lsp.enable("neocmake")
+vim.lsp.enable("zuban")
+vim.lsp.enable("rust_analyzer")
+vim.lsp.enable("taplo")
+vim.lsp.enable("tsgo")
+vim.lsp.enable("eslint")
+vim.lsp.enable("kotlin_lsp")
+vim.lsp.config("roslyn_ls", {
+	settings = {
+		["csharp|background_analysis"] = {
+			dotnet_analyzer_diagnostics_scope = "openFiles",
+			dotnet_compiler_diagnostics_scope = "openFiles",
+		},
+	},
+})
+vim.lsp.enable("roslyn_ls")
+vim.lsp.enable("yamlls")
+vim.lsp.enable("tailwindcss")
+vim.lsp.enable("html")
+vim.lsp.enable("nginx_language_server")
+vim.lsp.config["astro"] = {
 	init_options = {
 		typescript = {
 			tsdk = "/usr/lib/node_modules/typescript/lib",
 		},
 	},
-vim.lsp.enable("astro")
+}
+init_options = {
+	typescript = {
+		tsdk = "/usr/lib/node_modules/typescript/lib",
+	},
+}, vim.lsp.enable("astro")
 
-if vim.fn.has('win32') ~= 1 then
-	vim.lsp.enable('bashls')
+if vim.fn.has("win32") ~= 1 then
+	vim.lsp.enable("bashls")
 end
 
-vim.lsp.config('jsonls', {
+vim.lsp.config("jsonls", {
 	init_options = {
 		provideFormatter = false,
 	},
 })
-vim.lsp.enable('jsonls')
+vim.lsp.enable("jsonls")
