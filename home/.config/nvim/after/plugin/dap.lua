52ad8da38e223f8e002627a7fed421600d4fa374
Jithin Johnson <jithu7432@gmail.com>
2026-07-31 23:03:42 +0530

    feat: auto pid matching with lsof

--- home/.config/nvim/after/plugin/dap.lua
+++ home/.config/nvim/after/plugin/dap.lua
@@ -10,3 +10,17 @@ dap.adapters.coreclr = {
 	command = "netcoredbg",
 	args = { "--interpreter=vscode" },
 }
+
+if vim.fn.has("win32") ~= 1 then
+	local handle = vim.system({ "lsof", "-ti", ":8001" }, { text = true }):wait()
+	local pid_int = tonumber(vim.trim(handle.stdout)) or -1
+	dap.configurations.cs = {
+		{
+			type = "coreclr",
+			request = "attach",
+			name = "Attach to .NET (port)",
+			processId = pid_int,
+			justMyCode = true,
+		},
+	}
+end
